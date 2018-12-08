16e2422f59048916c9fef57b82662656532176b7
tenspot <mervyn.posnett@gmail.com>
2018-12-08 13:11:51 +0000

    update readline

--- FreeCCNodeExample/readline.js
+++ FreeCCNodeExample/readline.js
@@ -1,6 +1,34 @@
 // Example of Node's readline module
 
 const readline = require('readline');
-const rl = readline.createInterface({input: process.stdin, output: process.stout});
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
+let num1 = Math.floor((Math.random() * 10) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+let answer = num1 + num2
+
+rl.question(`What is ${ num1 } + ${ num2 }? \n`, 
+  (userInput => {
+    if(userInput.trim() == answer) {
+      rl.close();
+    } else {
+      rl.setPrompt('Incorrect response please try again\n');
+      rl.prompt();
+      // Listen for input event
+      rl.on('line', (userInput) => {
+        if(userInput.trim() == answer) {
+          rl.close();
+        } else {
+          rl.setPrompt(`Your answer of ${ userInput} is incorrect, try again\n`)
+          rl.prompt();
+        }
+      });
+    }
+  })
+);
+
+// Listen for readline close event emittted by rl.close()
+rl.on('close', () => {
+  console.log('correct!!!!!');
+});
 
