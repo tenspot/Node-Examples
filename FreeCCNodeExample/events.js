d8039f05d17a36794de243eb5ac72e0ca021b149
tenspot <mervyn.posnett@gmail.com>
2020-04-05 18:20:18 +0100

    update

--- FreeCCNodeExample/events.js
+++ FreeCCNodeExample/events.js
@@ -4,7 +4,7 @@ const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on('tutorial', (num1, num2) => {
-  console.log('tutoeial event has occured', num1+num2);
+  console.log('tutorial event has occured', num1+num2);
 });
 
 // Generate an event
